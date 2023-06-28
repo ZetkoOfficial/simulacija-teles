92ba28d19e1de056b471d1c3512d1d0042d6393e
ZetkoOfficial <info@zetkoofficial.com>
2023-06-29 00:09:08 +0200

    implementirana konstrukcija matrike R in Eulerjeva metoda za integracijo

--- simulacija.cpp
+++ simulacija.cpp
@@ -10,14 +10,17 @@
 using namespace std;
 using namespace Eigen;
 
+const double G = 1;
+
 struct body {
     double M;
     Vector3d x,v; SparseVector<double> m;
 
-    body(double M, Vector3d x, Vector3d v){
+    body(double M, Vector3d x, Vector3d v) {
         this->M = M; this->x = x; this->v = v;
     }
 
+    // funkcija nastavi masne vektorje za vsa telesa v seznamu bodies 
     static void populate_mass_vectors(vector<body>& bodies) {
         int N = bodies.size();
 
@@ -39,15 +42,59 @@ struct body {
     }
 };
 
+// funckija napolni stolpce R matrike z r(i,j)/|r(i,j)|^3 in vrne najmanjši kvadrat norme
+double fill_R_matrix(Matrix3Xd& R, const vector<body>& bodies) {
+    int N = bodies.size(); double min_norm = __DBL_MAX__;
+
+    int k = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            R.col(k) = bodies[j].x-bodies[i].x;
+            double norm = R.col(k).squaredNorm(); min_norm = min(min_norm, norm);
+            R.col(k) /= pow(norm, 1.5);
+            k++;
+        }
+    }
+
+    return min_norm;
+}
+
+// simulira gibanje teles za obdobje time, z standardnim korakom časa sdt in območjem previdnosti r(i,j) < danger_dist, ki dt zmanjša za danger_factor
+void simulate(vector<body>& bodies, Matrix3Xd& R, double time, double sdt = 1, double danger_dist = 10, double danger_factor=1000) {
+    int N = bodies.size();
+    double t = 0, next_t = 0;
+    while(t < time){
+        // preverimo če je čas za prikaz koordinat
+        if(t > next_t){
+             for(int j = 0; j < N; j++) cout << bodies[j].x.transpose() << endl;
+             next_t = t + sdt;
+        }
+
+        double norm = fill_R_matrix(R, bodies);
+        double dt = sdt;
+
+        // če je najbližja razdaja premajhna potem smo v nevarnem območju in zmanjšamo dt
+        if(norm <= danger_dist)  dt /= danger_factor;
+
+        // povečamo vektoje lege in hitrosti za vsa telesa za dxj in dvj
+        for(int j = 0; j < N; j++) {
+            bodies[j].x += bodies[j].v * dt;
+            bodies[j].v += G * (R*bodies[j].m) * dt;
+        }
+        t += dt;
+    }
+}
+
 int main() {
     vector<body> bodies = {
-        {1,{0,0,0},{0,0,0}},
-        {2,{0,0,0},{0,0,0}},
-        {3,{0,0,0},{0,0,0}},
-        {4,{0,0,0},{0,0,0}},
+        {1,{-10,0,0},{0,0.05,0.005}},
+        {1,{10,0,0},{0,-0.05,0}},
     };
 
-    body::populate_mass_vectors(bodies);
+    int N = bodies.size();
 
-    for(body& b : bodies) cout << VectorXd(b.m).transpose() << endl;
+    body::populate_mass_vectors(bodies);
+    Matrix3Xd R(3,(N*(N-1))/2);
+
+    simulate(bodies, R, 1000, 0.5, 50);
 }
